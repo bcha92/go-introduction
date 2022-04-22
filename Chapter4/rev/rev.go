e3aa9ea606c1691660cf719add2d5fb1a4f697c2
Brandon Cha <brandon.cha@redspace.com>
2022-04-22 12:40:09 -0400

    Chapters 2, 3, 4

---
+++ Chapter4/rev/rev.go
@@ -0,0 +1,37 @@
+// Reverse Reverses a slice of ints in place
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(a[:])
+	fmt.Println(a) // "[5 4 3 2 1 0]"
+
+	s := []int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions
+	reverse(s[:2])
+	reverse(s[2:])
+	reverse(s)
+	fmt.Println(s) // "[2 3 4 5 0 1]"
+}
+
+// Reverse array order
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// String equality checker
+func equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
